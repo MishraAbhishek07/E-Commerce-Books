6293c95edc87a9b695cd0b0f45814b6f73ba16dd
MishraAbhishek07 <davmishra962gmail.com>
2024-11-13 07:42:45 +0530

    Es-lint

--- src/pages/order/Order.jsx
+++ src/pages/order/Order.jsx
@@ -1,8 +1,8 @@
-import { OrderSuccess } from "./components/OrderSuccess";
-import { OrderFailed} from "./components/OrderFailed";
+
 import { useLocation } from "react-router-dom";
 import { useTitle } from "../../hooks/useTitle";
-
+import {OrderSuccess} from "./components/orderSuccess"
+import {OrderFailed} from "./components/OrderFailed"
 export const Order = () => {
   useTitle("Order-Summary")
   const { state } = useLocation();
