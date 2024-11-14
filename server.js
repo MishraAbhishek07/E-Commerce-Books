4eb9e1c9dae8ad1576f73e4026a8fc86ed3aef59
MishraAbhishek07 <davmishra962gmail.com>
2024-11-14 07:42:03 +0530

    updated

--- server.js
+++ server.js
@@ -28,28 +28,28 @@
 // server.listen(PORT, () => {
 //     console.log(`JSON Server is running on port ${PORT}`);
 // });
-import jsonServer from 'json-server';
-import jsonServerAuth from 'json-server-auth';
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import jsonServer from 'json-server';
+// import jsonServerAuth from 'json-server-auth';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'Data', 'db.json'));
-const middlewares = jsonServer.defaults();
+// const server = jsonServer.create();
+// const router = jsonServer.router(path.join(__dirname, 'Data', 'db.json'));
+// const middlewares = jsonServer.defaults();
 
-// Bind the router db to the app
-server.db = router.db;
+// // Bind the router db to the app
+// server.db = router.db;
 
-server.use(middlewares);
-server.use(jsonServerAuth); // Use json-server-auth for authentication
+// server.use(middlewares);
+// server.use(jsonServerAuth); // Use json-server-auth for authentication
 
-// Adding custom routes or middleware if needed
-server.use(router);
+// // Adding custom routes or middleware if needed
+// server.use(router);
 
-const PORT = 8000;
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
-});
+// const PORT = 8000;
+// server.listen(PORT, () => {
+//   console.log(`JSON Server is running on port ${PORT}`);
+// });
